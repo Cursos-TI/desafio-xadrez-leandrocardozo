e0348942ef36905b7c4151899e0a7617cbb87eca
Leandro C Braga <98498196+leandrocardozo@users.noreply.github.com>
2025-04-19 19:09:43 +0000

    Nível mestre concluído. Loops aninhados adicionado e funções recursivas.

--- xadrez.c
+++ xadrez.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
 
+//FUNÇÃO RECURSIVA DA PEÇA BISPO
+
+//a qtd de movimento é passada como argumento quando a funcao moverBispo(); é chamada 
+//e recebida como parâmetro aqui, ex: moverBispo(5);
+void moverBispo(int movimentosBispo){
+    if (movimentosBispo > 0){
+        printf(" DIAGONAL Á DIREITA.\n");
+        //decrementa um movimento até chegar em 0
+        moverBispo(movimentosBispo - 1);
+    }
+}
+
+void moverTorre(int movimentosTorre){
+    if(movimentosTorre > 0){
+        printf("DIREITA\n");
+        moverTorre(movimentosTorre - 1);
+    }
+}
+
+void moverRainha(int movimentosRainha){
+    if(movimentosRainha > 0){
+        printf("ESQUERDA\n");
+        moverRainha(movimentosRainha - 1);
+    }
+}
+
+
+
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
@@ -7,33 +35,33 @@
 int main()
 {
     // Nível Novato - Movimentação das Peças [Feito!]
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover. [Feito!]
     int bispo = 1;
     int rainha = 1;
     int movimentoCavalo = 1;
-    //=======================================================================================================
+    
 
     // Implementação de Movimentação do Bispo [Feito!]
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal. [Feito!]
     while (bispo <= 5)
     {
         printf("Movendo Bispo á [%dª] casa para diagonal á direita..\n", bispo);
         bispo++;
     }
     printf("\n\n\n\n");
-    //=======================================================================================================
+    
 
     // Implementação de Movimentação da Torre [Feito!]
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita. [Feito!]
     for (int torre = 1; torre <= 5; torre++)
     {
         printf("Movendo a Torre para [%dª] casa á direita..\n", torre);
     }
     printf("\n\n\n\n");
-    //=======================================================================================================
+    
 
     // Implementação de Movimentação da Rainha [Feito!]
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda. [Feito!]
     do
     {
         printf("Movendo a Rainha para [%dª] casa á esquerda..\n", rainha);
@@ -41,11 +69,11 @@ int main()
 
     } while (rainha <= 8);
     printf("\n\n\n\n");
-    //=======================================================================================================
+    
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    // Nível Aventureiro - Movimentação do Cavalo [Feito!]
+    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L. [Feito!]
+    // Um loop pode representar a movimentação horizontal e outro vertical. [Feito!]
     int jogar = 4;
     while (jogar >= 1)
     {
@@ -127,19 +155,63 @@ int main()
         }
         else
         {
-            printf("!!!!!! OPÇÃO INVÁLIDA !!!!!!!\n");
+            printf("!!!!!! OPÇÃO INVÁLIDA !!!!!!!\n\n");
+            continue;
+            
+
         }
 
         jogar--;
     }
-    printf("****** ACABARAM SUAS JOGADAS ******\n\n\n");
-    //=======================================================================================================
+    printf("****** ACABARAM SUAS JOGADAS ******\n\n\n\n\n");
+    
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
+
+
+    // Sugestão: Substitua as movimentações das peças por funções recursivas. [Feito!]
+    // Exemplo: Crie uma função recursiva para o movimento do Bispo. [Feito!]
+
+    /*
+   
+    void moverBispo(int movimentosBispo){
+        if (movimentosBispo > 0){
+            printf(" DIAGONAL Á DIREITA.\n");
+            //decrementa um movimento até chegar em 0
+            moverBispo(movimentosBispo - 1);
+        }
+    }
+
+    void moverTorre(int movimentosTorre){
+        if(movimentosTorre > 0){
+            printf("DIREITA\n");
+            moverTorre(movimentosTorre - 1);
+        }
+    }
+
+    void moverRainha(int movimentosRainha){
+        if(movimentosRainha > 0){
+            printf("ESQUERDA\n");
+            moverRainha(movimentosRainha - 1);
+        }
+    }
+        
+    */
+    printf("------EXECUTANDO O MOVIMENTO DE TODAS AS PEÇAS COM FUNÇÃO RECURSIVA------\n\n");
+
+    moverBispo(5); //chamando a função recursiva de moverBispo() declarada antes do int main()
+    printf("\n\n");
+    moverTorre(5); //chamando a função recursiva de moverTorre() declarada antes do int main()
+    printf("\n\n");
+    moverRainha(8); //chamando a função recursiva de moverRainha() declarada antes do int main()
+
+
+
+
+    
+
+    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.  
     // Inclua o uso de continue e break dentro dos loops.
 
     return 0;
